a58b014f6112f86383692bcf3cace88b65d1cfec
q-13t <vova29092002@gmail.com>
2024-10-27 11:22:04 +0200

    IMP: axiosConfig

---
+++ src/api/axiosConfig.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: "",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    withCredentials: true,
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+// TODO: configure response interceptor
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default api;
