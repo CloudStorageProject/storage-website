da93db0f2e4456d020800c5aadc0c5690936a9ab
q-13t <vova29092002@gmail.com>
2024-10-27 14:02:43 +0200

    IMP: Routes & AuthProvider

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@
 import './App.css';
 import LoginPage from './components/loginPage/loginPage';
 import RegistrationPage from './components/registrationPage/registrationPage';
-
+import RequireAuth from '@auth-kit/react-router/RequireAuth';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <LoginPage/>
-      <RegistrationPage/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/register" element={<RegistrationPage />} />
+                    <Route path="/" element={<RequireAuth fallbackPath="/login"></RequireAuth>} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AuthProvider from 'react-auth-kit';
+import createStore from 'react-auth-kit/createStore';
 
+const store = createStore({
+    authName: '_auth',
+    authType: 'cookie',
+    cookieDomain: window.location.hostname,
+    cookieSecure: window.location.protocol === 'https:'
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <AuthProvider store={store}>
+            <App />
+        </AuthProvider>
+    </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
