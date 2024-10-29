d26fd284e9e96a2b30ab1d796fb68fd4c2f3e303
q-13t <vova29092002@gmail.com>
2024-10-29 12:22:04 +0200

    FIX: useContext

--- src/api/authRequests.jsx
+++ src/api/authRequests.jsx
@@ -1,14 +1,15 @@
 import AxiosInstance from "./axiosConfig";
 
-const api = AxiosInstance();
 
 export const loginRequest = async (data) => {
+    const api = AxiosInstance();
     const res = await api.post("/login", data);
     return JSON.parse(res.data);
 };
 
 
 export const registerRequest = async (data) => {
+    const api = AxiosInstance();
     const res = await api.post("/register", data);
     return JSON.parse(res.data);
 }
