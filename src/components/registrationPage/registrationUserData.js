1be15d3bcde9a376a34fd10d3a1fc51027185a82
q-13t <vova29092002@gmail.com>
2024-12-24 13:43:19 +0200

    UPD: Credential Limitations & Data Preservation

--- src/components/registrationPage/registrationUserData.js
+++ src/components/registrationPage/registrationUserData.js
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./registrationUserData.js"
 import { useState } from "react";
 import bgimg from '../img/greenBackroundLoginPage.jpg'
 
-const RegistrationUserData = ({userData, setUserData, nextStage, previousStage}) => {
-    const [formData, setFormData] = useState(userData);
-  
+const RegistrationUserData = ({userData, setUserData, nextStage}) => {
+    let [formData, setFormData] = useState(userData);
+    const userNameRegex = /^[a-zA-Z0-9]+$/
+    
     const handleChange = (e) => {
-        if (e.target.name === "confirmPassword") {
-            if (e.target.value !== formData.password) {
-                // TODO: handle password mismatch
-            }
+        e.preventDefault();
+        if (e.target.name === "confirmPassword" && (e.target.value !== userData.password)) {
+            // TODO: handle password mismatch
+        }else if (e.target.name === "username" && !userNameRegex.test(e.target.value)) {
+            // TODO: handle invalid username
+        } else if (e.target.name === "password" && (e.target.value.length < 8 || e.target.value.length > 128)  ) {
+            // TODO: handle password length
         }
-        setFormData({ ...formData, [e.target.name]: e.target.value });
-        setUserData({ ...userData, [e.target.name]: e.target.value });
-    }
+        setFormData(formData => ({ ...formData, [e.target.name]: e.target.value }));
+    };
+
+    useEffect(() => {
+        setUserData(formData);
+    }, [formData]);
 
 
     return (
@@ -23,9 +30,9 @@ const RegistrationUserData = ({userData, setUserData, nextStage, previousStage})
                 <form className="login-form">
                     <h2 className="login-title">Registration</h2>
                     <p className="desc">It’s completely free</p>
-                    <input type="text" placeholder="Name" className="login-input" name="name" value={formData.name} onChange={handleChange} />
-                    <input type="text" placeholder="Username" className="login-input" name="username" value={formData.useState} onChange={handleChange} />
-                    <input type="email" placeholder="Email" className="login-input" name="email" value={formData.email} onChange={handleChange} />
+                    <input type="text" placeholder="Name" className="login-input" name="name" value={formData.name} onChange={(e)=>{handleChange(e)}} />
+                    <input type="text" placeholder="Username" className="login-input" name="username" value={formData.username} onChange={(e)=>{handleChange(e)}} />
+                    <input type="email" placeholder="Email" className="login-input" name="email" value={formData.email} onChange={(e)=>{handleChange(e)}} />
                     <input type="password" placeholder="Password" className="login-input" name="password" onChange={handleChange} />
                     <input type="password" placeholder="Confirm Password" className="login-input" name="confirmPassword" onChange={handleChange} />
                     <button type="submit" className="login-button" onClick={nextStage}>
