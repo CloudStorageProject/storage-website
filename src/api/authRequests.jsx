8766d3cd9990b75f3e3ad07bab8e21e09a0ad4a2
q-13t <vova29092002@gmail.com>
2025-01-09 15:43:20 +0200

    UPD: AuthRequests

--- src/api/authRequests.jsx
+++ src/api/authRequests.jsx
@@ -1,5 +1,5 @@
 import { axiosInstanceJSON } from "./axiosConfig";
-
+import { exportPublicKeyToBase64 } from "../utils/Cryptography";
 
 export const loginRequest = async (data) => {
     let user_data = { username: data.username, password: data.password };
@@ -10,21 +10,22 @@ export const loginRequest = async (data) => {
 
 export const registerRequest = async (data) => {
     let user_data = {
-        name: data.name,
-        email: data.email,
         username: data.username,
-        password: data.password
+        email: data.email,
+        password: data.password,
+        publicKey: data.publicKey
     }
     const res = await axiosInstanceJSON.post("/register", user_data);
     return res;
 };
 
 export const requestChallenge = async (publicKey) => {
-    const res = await axiosInstanceJSON.get("/auth/login/challenge/" + publicKey);
+    return { message: "This is some data" };
+    const res = await axiosInstanceJSON.get("/auth/login/challenge/" + exportPublicKeyToBase64(publicKey));
     return res;
 };
 
 export const submitChallenge = async (publicKey, data) => {
-    const res = await axiosInstanceJSON.post("/auth/login/challenge/" + publicKey, data);
+    const res = await axiosInstanceJSON.post("/auth/login/challenge/" + exportPublicKeyToBase64(publicKey), data);
     return res;
 };
