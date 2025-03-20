d0d4b94f530138885ec3400cce4cee209975f6d4
q-13t <vova29092002@gmail.com>
2025-03-20 14:54:15 +0200

    IMP: BredCrump Navigation

--- src/components/mainPage/elements/Folder.jsx
+++ src/components/mainPage/elements/Folder.jsx
@@ -2,16 +2,10 @@ import { ReactComponent as FolderIcon } from "../../img/Folder.svg";
 import "./elements.css";
 import { ReactComponent as MoreIcon } from "../../img/More.svg";
 
-const Folder = ({ folder, viewMode, page, setCurrentFolder }) => {
-
-
-    const handleFolderSelect = () => {
-        setCurrentFolder(folder);
-        page.setPageState({ ...page.pageState, currentFolder: folder, toUpdate: !page.pageState.toUpdate });
-    }
+const Folder = ({ folder, viewMode, changeCurrentFolder }) => {
 
     return (
-        <div key={folder.id} className={viewMode === "gallery" ? "folder-grid" : "folder-list"} onDoubleClick={() => { handleFolderSelect(); }}>
+        <div key={folder.id} className={viewMode === "gallery" ? "folder-grid" : "folder-list"} onDoubleClick={() => { changeCurrentFolder(folder); }}>
             <div className="folder">
                 <div className="info">
                     <FolderIcon />
