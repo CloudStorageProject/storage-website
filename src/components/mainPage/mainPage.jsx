0d183b42c64df2a190c018f077855cec7fe4b3fd
q-13t <vova29092002@gmail.com>
2025-05-16 15:29:23 +0300

    FIX: categories.css

--- src/components/mainPage/mainPage.jsx
+++ src/components/mainPage/mainPage.jsx
@@ -4,6 +4,7 @@ import "./mainpage.css";
 import MyDisk from "./categories/MyDisk";
 import OpenedToMe from "./categories/OpenedToMe";
 import ProfileSettings from "./categories/ProfileSettings.jsx";
+import './categories/categories.css'
 
 export default class MainPage extends React.Component {
     constructor(props) {
