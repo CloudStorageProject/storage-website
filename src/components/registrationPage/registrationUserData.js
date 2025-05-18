8527cd9a94222ff9dca2b961cad54c9b019fdd52
q-13t <vova29092002@gmail.com>
2025-05-18 12:08:21 +0300

    FIX: Registration: Name Field

--- src/components/registrationPage/registrationUserData.js
+++ src/components/registrationPage/registrationUserData.js
@@ -15,6 +15,7 @@ const RegistrationUserData = ({ userData, setSecrets, setUserData, nextStage }) 
     const notify = useNotify();
 
     const checkUserData = (data) => {
+        // TODO: check if username is already taken
         if (!emailRegex.test(data.email)) {
             notify.postNotification("Please enter a valid email", NotificationType.WARNING);
             return false;
@@ -69,7 +70,6 @@ const RegistrationUserData = ({ userData, setSecrets, setUserData, nextStage }) 
                 <form className="login-form">
                     <h2 className="login-title">Registration</h2>
                     <p className="desc">It’s completely free</p>
-                    <input type="text" placeholder="Name" className="login-input" name="name" value={formData.name} onChange={(e) => { handleChange(e) }} />
                     <input type="text" placeholder="Username" className="login-input" name="username" value={formData.username} onChange={(e) => { handleChange(e) }} />
                     <input type="email" placeholder="Email" className="login-input" name="email" value={formData.email} onChange={(e) => { handleChange(e) }} />
                     <input type="password" placeholder="Password" className="login-input" name="password" onChange={handleChange} />
